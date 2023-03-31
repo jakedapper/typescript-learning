9a5d01568b61103c4b89909b3accbf9c1f902af2
jake dapper <jakedapper@gmail.com>
2023-03-31 10:18:16 -0500

    almost done w variables

---
+++ functions.ts
@@ -0,0 +1,49 @@
+// Function parameter type annotations:
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
+  //doSomething(must put variables in correct order - especially bc of the whole type thing)
+
+// Return type annotation:
+function greet(person: string = "stranger"): string {
+  return `Hi there, ${person}!`;
+}
+//default parameter included above ^
+//we are declaring variable type AND return type
+  //typescript can infer return type - aka not necessary - this annotation isis after our parameter annotations outside the parentheses
+  
+
+function square(num: number): number {
+  return num * num;
+}
+
+square(3);
+greet("Tonya Harding");
+doSomething("ChickenFace", 78, true);
+
+// Arrow function:
+const add = (x: number, y: number): number => {
+  return x + y;
+};
+
+// Contextual Type Clues
+const colors = ["red", "orange", "yellow"];
+colors.map((color) => {
+  return color.toUpperCase();
+});
+
+// Void
+function printTwice(msg: string): void {
+  console.log(msg);
+  console.log(msg);
+}
+
+// Never
+function makeError(msg: string): never {
+  throw new Error(msg);
+}
+
+function gameLoop(): never {
+  while (true) {
+    console.log("GAME LOOP RUNNING!");
+  }
+}
---
+++ random misc.md
@@ -0,0 +1,4 @@
+tsc filename.ts compiles it into a JS file
+
+type inference v. type annotation
+-letting typescript infer the type vs. explicitly annotating its type
